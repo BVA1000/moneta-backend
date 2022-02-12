7fecb8d73b8fdc73eb901e467be43c65b0b19fa4
BenVonAchen <bennyvonachen@gmail.com>
2022-02-12 13:04:36 -0700

    Setting up first routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET posts listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Not the final stage, brainstroming and getting starter ideas for Moneta Backend
+router.get("/", function (req, res, next) {
+  res.status(200).json({ message: "Create Posts" });
+});
+
+router.post("/", function (req, res, next) {
+  res.status(200).json({ message: "Upload Posts" });
+});
+
+router.put("/:id", function (req, res, next) {
+  res.status(200).json({ message: `Update Posts ${req.params.id}` });
+});
+
+router.delete("/:id", function (req, res, next) {
+  res.status(200).json({ message: `Delete Posts ${req.params.id}` });
 });
 
 module.exports = router;
